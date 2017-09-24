74c3b7353479f02a264bded26f7eb3d706df7d8d
Eiji Onchi <onchi.eiji@gmail.com>
2017-09-24 18:33:38 +0900

    Renamed the example file

---
+++ examples/CGx_InverseK_Example/CGx_InverseK_Example.ino
@@ -0,0 +1,63 @@
+// Arduino/Teensy example for Arduino Braccio
+
+// Include the library InverseK.h
+#include <InverseK.h>
+
+void setup() {
+  // Setup the lengths and rotation limits for each link
+  Link base, upperarm, forearm, hand;
+  
+  base.init(0, b2a(0.0), b2a(180.0));
+  upperarm.init(200, b2a(15.0), b2a(165.0));
+  forearm.init(200, b2a(0.0), b2a(180.0));
+  hand.init(270, b2a(0.0), b2a(180.0));
+
+  // Attach the links to the inverse kinematic model
+  InverseK.attach(base, upperarm, forearm, hand);
+
+  float a0, a1, a2, a3;
+
+  // InverseK.solve() return true if it could find a solution and false if not. 
+  
+  // Calculates the angles without considering a specific approach angle
+  // InverseK.solve(x, y, z, a0, a1, a2, a3)
+  if(InverseK.solve(200, 100, 100, a0, a1, a2, a3)) {
+    Serial.print(a2b(a0)); Serial.print(',');
+    Serial.print(a2b(a1)); Serial.print(',');
+    Serial.print(a2b(a2)); Serial.print(',');
+    Serial.println(a2b(a3));
+  } else {
+    Serial.print(a2b(0)); Serial.print(',');
+    Serial.print(a2b(0)); Serial.print(',');
+    Serial.print(a2b(0)); Serial.print(',');
+    Serial.println(a2b(0));
+  }
+  
+  // Calculates the angles considering a specific approach angle 'phi'
+  // InverseK.solve(x, y, z, a0, a1, a2, a3, phi)
+  if(InverseK.solve(200, 100, 100, a0, a1, a2, a3, 0.0)) {
+    Serial.print(a2b(a0)); Serial.print(',');
+    Serial.print(a2b(a1)); Serial.print(',');
+    Serial.print(a2b(a2)); Serial.print(',');
+    Serial.println(a2b(a3));
+  } else {
+    Serial.print(a2b(0)); Serial.print(',');
+    Serial.print(a2b(0)); Serial.print(',');
+    Serial.print(a2b(0)); Serial.print(',');
+    Serial.println(a2b(0));
+  }
+}
+
+void loop() {
+  
+}
+
+// Quick conversion from the Braccio angle system to radians 
+float b2a(float b){ 
+  return b / 180.0 * PI - HALF_PI;
+}
+
+// Quick conversion from radians to the Braccio angle system
+float a2b(float a) { 
+  return (a + HALF_PI) * 180 / PI;
+}
