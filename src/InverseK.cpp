a06aaa96cec01e443c7d04c5e1ea85428540bbff
Eiji Onchi <onchi.eiji@gmail.com>
2017-10-02 16:39:15 +0900

    Added license header

--- src/InverseK.cpp
+++ src/InverseK.cpp
@@ -1,8 +1,19 @@
-/*
-  InverseK.cpp - Library for processing inverse kinematics of a 3-link robotic arm.
-  Created by Eiji Onchi, September 24, 2017.
-  Released into the public domain.
-*/
+/* 
+ * This file is part of the CGx-InverseK distribution (https://github.com/cgxeiji/CGx-InverseK).
+ * Copyright (c) 2017 Eiji Onchi.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "math.h"
 #include "InverseK.h"
